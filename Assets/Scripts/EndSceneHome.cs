98266d66b4923bd465bf62b08f337cb267d8b62c
Sabrina Mackay <61383839+SabrinaMackay@users.noreply.github.com>
2020-09-01 13:05:33 +0200

    Update EndSceneHome.cs

--- Assets/Scripts/EndSceneHome.cs
+++ Assets/Scripts/EndSceneHome.cs
@@ -1,4 +1,4 @@
-﻿using System.Collections;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
@@ -9,10 +9,10 @@ public class EndSceneHome : MonoBehaviour
 	private float nextFrame = 6f;
 	private int counter = 1;
 
-	private Vector3 frameStart = new Vector3(275f,-2.79f,-10f);
-	private Vector3 frameOne = new Vector3(290f, -2.79f, -10f);
-	private Vector3 frameTwo = new Vector3(320f, -2.79f, -10f);
-	private Vector3 frameThree = new Vector3(347f, -2.79f, -10f);
+	private Vector3 frameStart = new Vector3(265f,-2.79f,-10f);
+	private Vector3 frameHouse = new Vector3(290f, -2.79f, -10f);
+	private Vector3 frameBathroom = new Vector3(320f, -2.79f, -10f);
+	private Vector3 frameLaundry = new Vector3(347f, -2.79f, -10f);
 
     // Start is called before the first frame update
     void Start()
@@ -24,34 +24,21 @@ public class EndSceneHome : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-
-        if(transform.position.x>frameStart.x){
-            transform.position = frameStart;
-        	if(counter<5)
-        {
-            sceneTimer += Time.deltaTime;
-        }
-            if(sceneTimer >= nextFrame)
+        if((transform.position.x >= frameStart.x) && (transform.position.x < frameHouse.x))
             {
-            	counter++;
-            	switch(counter){
-            		case 2:
-            				transform.position = Vector3.Lerp(frameStart, frameOne, 1f);
-            				break;
-    				case 3:
-    						transform.position = Vector3.Lerp(frameOne, frameTwo, 1f);
-            				break;
-                    case 4:
-                            
-                            transform.position = Vector3.Lerp(frameTwo, frameThree, 1f);
-                            break;
-            		default:
-            				print("error");
-            				break;
-            	}
-            	//transform.position = frame;
-            	sceneTimer = startScene;
+                transform.position = frameHouse;
             }
-        }
+        if(transform.position.x >= frameStart.x)
+        {sceneTimer += Time.deltaTime;}
+        if(sceneTimer >= nextFrame)
+            {
+                sceneTimer = startScene;
+                if(counter == 1)
+                    {transform.position = frameBathroom;}
+                if(counter == 2)
+                    {transform.position = frameLaundry;}
+                counter++;
+            }
     }
+    
 }
